408e1ae88cf103e679f656a3af440d5ec1fdf820
wei <wiwei032835@gmail.com>
2025-07-26 23:40:41 +0800

    update code

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     vueDevTools(),
@@ -16,3 +17,4 @@ export default defineConfig({
     },
   },
 })
+
