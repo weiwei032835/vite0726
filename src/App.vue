cd36509a4a4ac68d2afbe1ec49fcb373c982c7da
wei <wiwei032835@gmail.com>
2025-07-26 21:16:52 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import { ref } from 'vue';
+
+  const num = ref(2);
+  const clickMe = () => {
+    num.value++;
+  };
+</script>
+
+
+<template>
+  {{num}}
+  <button type="button" v-on:click="clickMe">點我</button>
+</template>
+
